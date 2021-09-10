70da50b739bb2bca9b8b44f14068134450ae4a61
srkrishna3384 <srkrishna3384@gmail.com>
2021-09-10 09:58:36 +0530

    Added SepProject

---
+++ src/test/java/com/sample/test/TestDemo.java
@@ -0,0 +1,28 @@
+package com.sample.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestDemo {
+	@Test
+	public static void browserLaunch() {
+		//System.setProperty(key, value)
+//		WebDriverManager.chromedriver().setup();
+//		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("TestDemo");
+		System.out.println();
+		System.out.println("TestDemo");
+		System.out.println();
+		System.out.println("TestDemo");
+		System.out.println();
+		System.out.println("TestDemo");
+		System.out.println();
+		System.out.println("TestDemo");
+		
+	}
+
+}
