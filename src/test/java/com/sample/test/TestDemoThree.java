ec5dc0f0ef52b560d243abbab7d75976a0784627
Rama <sriramula_k_rama@optum.com>
2021-09-14 08:35:39 +0530

    Added TestDemoThree by Person2

---
+++ src/test/java/com/sample/test/TestDemoThree.java
@@ -0,0 +1,20 @@
+package com.sample.test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestDemoThree {
+	@Test
+	public static void browserLaunch() {
+		
+		System.out.println("TestDemoThree");
+		System.out.println();
+		System.out.println("TestDemoThree");
+		System.out.println();
+		
+	}
+
+}
